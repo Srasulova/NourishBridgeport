3b113da134b440253d51eb59ceac7e1d782bb5e3
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-09-26 01:47:55 -0400

    added components to page

--- nourish-bridgeport/src/app/page.tsx
+++ nourish-bridgeport/src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image'
 import NavBar from '@/components/navbar'
+import HeroArea from '@/components/hero-area'
+import AboutUs from '@/components/about-us'
+import OurPrograms from '@/components/programs'
 
 export default function Home() {
   return (
     <main className="">
       <NavBar />
+      <HeroArea />
+      <AboutUs />
+      <OurPrograms />
     </main>
   )
 }
