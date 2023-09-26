9bd574f40e8d3f933723c4d5e8f7a18cc086698e
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-09-26 01:47:34 -0400

    navbar styling changes

--- nourish-bridgeport/src/components/navbar.tsx
+++ nourish-bridgeport/src/components/navbar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image"
+import logo from "../../public/logo.png"
 
 export default function NavBar() {
     return (
-        <nav className="bg-white shadow">
+        <nav className="bg-white shadow-lg w-full">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 justify-between">
+                <div className="relative flex h-auto justify-around">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* <!-- Mobile menu button --> */}
                         <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" aria-controls="mobile-menu" aria-expanded="false">
@@ -28,55 +29,22 @@ export default function NavBar() {
                             </svg>
                         </button>
                     </div>
-                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
                         <div className="flex flex-shrink-0 items-center">
-                            <Image className="h-8 w-auto" src="" alt="Your Company" />
+                            <Image className="h-32 sm:h-24 w-auto m-2" src={logo} alt="Your Company" />
                         </div>
                         <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-                            {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" --> */}
-                            <a href="#" className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900">Dashboard</a>
-                            <a href="#" className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Team</a>
-                            <a href="#" className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Projects</a>
-                            <a href="#" className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700">Calendar</a>
+                            {/* <!-- Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-600 hover:text-gray-700" --> */}
+                            <a href="#" className="inline-flex items-center  underline decoration-2 decoration-red-600 hover:underline hover:decoration-2  hover:decoration-green-600 hover:text-gray-700 px-1 pt-1  font-medium text-gray-900">Home</a>
+                            <a href="#" className="inline-flex items-center  px-1 pt-1  font-medium text-gray-500 hover:underline hover:decoration-2 hover:decoration-green-600 hover:text-gray-700">About Us</a>
+                            <a href="#" className="inline-flex items-center  px-1 pt-1  font-medium text-gray-500 hover:underline hover:decoration-2 hover:decoration-green-600 hover:text-gray-700">Programs</a>
+                            <a href="#" className="inline-flex items-center  px-1 pt-1  font-medium text-gray-500 hover:underline hover:decoration-2 hover:decoration-green-600 hover:text-gray-700">Donate</a>
+                            <a href="#" className="inline-flex items-center  px-1 pt-1  font-medium text-gray-500 hover:underline hover:decoration-2 hover:decoration-green-600 hover:text-gray-700">Volunteer</a>
+                            <a href="#" className="inline-flex items-center  px-1 pt-1  font-medium text-gray-500 hover:underline hover:decoration-2 hover:decoration-green-600 hover:text-gray-700">Events</a>
+                            <a href="#" className="inline-flex items-center  px-1 pt-1  font-medium text-gray-500 hover:underline hover:decoration-2 hover:decoration-green-600 hover:text-gray-700">Contact Us</a>
                         </div>
                     </div>
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                            <span className="absolute -inset-1.5"></span>
-                            <span className="sr-only">View notifications</span>
-                            <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                            </svg>
-                        </button>
 
-                        {/* <!-- Profile dropdown --> */}
-                        <div className="relative ml-3">
-                            <div>
-                                <button type="button" className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                    <span className="absolute -inset-1.5"></span>
-                                    <span className="sr-only">Open user menu</span>
-                                    <Image className="h-8 w-8 rounded-full" src="" alt="" />
-                                </button>
-                            </div>
-
-                            {/* <!--
-                        Dropdown menu, show/hide based on menu state.
-
-                        Entering: "transition ease-out duration-200"
-                        From: "transform opacity-0 scale-95"
-                        To: "transform opacity-100 scale-100"
-                        Leaving: "transition ease-in duration-75"
-                        From: "transform opacity-100 scale-100"
-                        To: "transform opacity-0 scale-95"
-          --> */}
-                            <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
-                                {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-0">Your Profile</a>
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-1">Settings</a>
-                                <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex={-1} id="user-menu-item-2">Sign out</a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
 
@@ -84,10 +52,13 @@ export default function NavBar() {
             <div className="sm:hidden" id="mobile-menu">
                 <div className="space-y-1 pb-4 pt-2">
                     {/* <!-- Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" --> */}
-                    <a href="#" className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700">Dashboard</a>
-                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Team</a>
-                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Projects</a>
-                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700">Calendar</a>
+                    <a href="#" className="block border-l-4 border-green-600 bg-green-50 py-2 pl-3 pr-4 text-base font-medium text-green-700">Home</a>
+                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-600 hover:bg-gray-50 hover:text-gray-700">About Us</a>
+                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-600 hover:bg-gray-50 hover:text-gray-700">Programs</a>
+                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-600 hover:bg-gray-50 hover:text-gray-700">Donate</a>
+                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-600 hover:bg-gray-50 hover:text-gray-700">Volunteer</a>
+                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-600 hover:bg-gray-50 hover:text-gray-700">Events</a>
+                    <a href="#" className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-600 hover:bg-gray-50 hover:text-gray-700">Contact us</a>
                 </div>
             </div>
         </nav>
