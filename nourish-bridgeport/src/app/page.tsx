412dffaee04bb5e72a681dcea301a188babd8dd8
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-09-28 01:37:49 -0400

    add announcement and events component

--- nourish-bridgeport/src/app/page.tsx
+++ nourish-bridgeport/src/app/page.tsx
@@ -3,6 +3,7 @@ import NavBar from '@/components/navbar'
 import HeroArea from '@/components/hero-area'
 import AboutUs from '@/components/about-us'
 import OurPrograms from '@/components/programs'
+import Events from '@/components/events'
 
 export default function Home() {
   return (
@@ -11,6 +12,10 @@ export default function Home() {
       <HeroArea />
       <AboutUs />
       <OurPrograms />
+      <div className='py-12 w-4/5 border-b-2 border-red-500 mx-auto mb-10'>
+        <p className='text-center text-3xl font-semibold text-gray-800'>Ready to jump in? <span className='text-red-600'> Visit our Volunteer Page.</span> </p>
+      </div>
+      <Events />
     </main>
   )
 }
