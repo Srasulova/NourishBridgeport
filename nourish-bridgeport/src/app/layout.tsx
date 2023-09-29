6fc0920651831200dc47a05f7afa7b575158976b
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-09-29 01:42:16 -0400

    placed navbar and footer in the layout

--- nourish-bridgeport/src/app/layout.tsx
+++ nourish-bridgeport/src/app/layout.tsx
@@ -1,8 +1,16 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Heebo } from 'next/font/google'
+import NavBar from '@/components/navbar'
+import Footer from '@/components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const heebo = Inter({
+  subsets: ['latin'],
+  variable: '--font-heebo',
+})
 
 export const metadata: Metadata = {
   title: 'nOURish Bridgeport - Nourish Bridgeport Inc - NourishBPT.ORG',
@@ -16,7 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${heebo.variable} font-sans`}>
+        <NavBar />
+        {children}</body>
+      <Footer />
     </html>
   )
 }
