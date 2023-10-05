44cbbf4dc9b8ec0b034fa4b6878f04f9b52d94fc
Sabina <109144680+Srasulova@users.noreply.github.com>
2023-10-05 02:18:17 -0400

    modified the button

--- nourish-bridgeport/src/components/programs.tsx
+++ nourish-bridgeport/src/components/programs.tsx
@@ -47,9 +47,23 @@ export default function OurPrograms() {
                 </div>
             </div>
 
-            <div className="flex justify-center mt-10">
-                <button className="text-red-600 hover:text-red-700 hover:font-bold hover:drop-shadow-md font-semibold text-lg">More about our programs &darr;</button>
+            <div className="flex justify-center my-10 btn-div">
+                <button className="">
+                    <a href="#_" className="relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-red-600 transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-white group shadow-md">
+                        <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-red-600 group-hover:h-full"></span>
+                        <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
+                            <svg className="w-5 h-5 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+
+                        </span>
+                        <span className="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
+                            <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                        </span>
+                        <span className="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-white">More about our programs</span>
+                    </a>
+                </button>
             </div>
+
+
         </div>
     </>)
 }
